1906982979d900292295760969f8bf35db572d28
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-28 20:19:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
 class Application
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/search/)
-      item_name = req.path.split("/search/")
+      item_name = req.path.split("/search/").last
+      item = Item.all.find
     else
       resp.write "Route not found"
       resp.status = 404
