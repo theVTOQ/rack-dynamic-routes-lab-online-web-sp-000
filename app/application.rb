86a4740c08a8a40263fe240f68aa1689c7703751
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-28 20:15:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,11 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if req.path.match(/search/)
+
+    else
+      resp.status = 404
   end
 end
